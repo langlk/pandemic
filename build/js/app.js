354c4c98c12cca0eb76e248cfc79283f0243a101
Kelsey Langlois and LDMacKrell <ldmackrell@>
2017-10-17 14:32:04 -0700

    Update UI to update once per time unit.

--- build/js/app.js
+++ build/js/app.js
@@ -264,12 +264,20 @@ var Location = exports.Location = function () {
   _createClass(Location, [{
     key: "infest",
     value: function infest(infestation) {
+      this.infestHelp(infestation, []);
+    }
+  }, {
+    key: "infestHelp",
+    value: function infestHelp(infestation, maxed) {
       if (this.infestationAmounts[infestation] < 3) {
         this.infestationAmounts[infestation] += 1;
       } else {
         // Outbreak!
+        maxed.push(this);
         this.nextDoor.forEach(function (location) {
-          location.infest(infestation);
+          if (!maxed.includes(location)) {
+            location.infestHelp(infestation, maxed);
+          }
         });
       }
     }
@@ -374,49 +382,94 @@ var Player = exports.Player = function () {
 
 var _game = require("./../js/game.js");
 
+var timeUnit = 500;
+
 function IDify(string) {
   string = string.split(" ").join("-");
   return string;
 }
 
-$(document).ready(function () {
-  var game = new _game.Game(15000);
+function clearUI() {
+  $(".options").html("");
+  $("ul ul").html("");
+  $(".player-location").removeClass("player-location");
+}
+
+function updateUI(game) {
+  console.log(game);
+  clearUI();
+  // Show infestation amounts for each location
   Object.keys(game.locations).forEach(function (name) {
-    $('.locations').append("<li id=" + IDify(name) + ">" + name + "<div class=\"options\"> </div></li>");
+    var location = game.locations[name];
+    Object.keys(location.infestationAmounts).forEach(function (infestation) {
+      if (location.infestationAmounts[infestation] > 0) {
+        $("#" + IDify(name) + " ul").append("<li>" + infestation + ": " + location.infestationAmounts[infestation] + "</li>");
+      }
+    });
+  });
+  var playerLocation = game.player.location;
+  // Highlight player location
+  $("#" + IDify(playerLocation.name)).addClass("player-location");
+  // Add Move option to next door locations
+  playerLocation.nextDoor.forEach(function (location) {
+    $("#" + IDify(location.name) + " .options").append("<button type=\"button\" id=\"move-" + IDify(location.name) + "\">Move</button>");
+  });
+  // Create Cure button for player location
+  var disabled = "disabled";
+  if (game.player.treated[playerLocation.infestationDefault]) {
+    disabled = null;
+  }
+  $("#" + IDify(playerLocation.name) + " .options").append("<button type=\"button\" id=\"cure\" " + disabled + ">Cure " + playerLocation.infestationDefault + "</button>");
+  // Create Treat button for any present infestations
+  Object.keys(playerLocation.infestationAmounts).forEach(function (infestation) {
+    if (playerLocation.infestationAmounts[infestation] > 0) {
+      $("#" + IDify(playerLocation.name) + " .options").append("<button type=\"button\" id=\"treat-" + IDify(infestation) + "\">Treat " + infestation + "</button>");
+    }
+  });
+}
+
+$(document).ready(function () {
+  var game = new _game.Game(timeUnit);
+  Object.keys(game.locations).forEach(function (name) {
+    $('.locations').append("<li id=" + IDify(name) + ">" + name + "<div class=\"options\"> </div><ul></ul></li>");
   });
 
   $('#game-start').click(function () {
     game.start();
     console.log(game);
+    updateUI(game);
+    setInterval(function () {
+      updateUI(game);
+    }, timeUnit);
     // Show infestation amounts for each location
-    Object.keys(game.locations).forEach(function (name) {
-      var location = game.locations[name];
-      $("#" + IDify(name)).append("<ul></ul>");
-      Object.keys(location.infestationAmounts).forEach(function (infestation) {
-        if (location.infestationAmounts[infestation] > 0) {
-          $("#" + IDify(name) + " ul").append("<li>" + infestation + ": " + location.infestationAmounts[infestation] + "</li>");
-        }
-      });
-    });
-    var playerLocation = game.player.location;
-    // Highlight player location
-    $("#" + IDify(playerLocation.name)).addClass("player-location");
-    // Add Move option to next door locations
-    playerLocation.nextDoor.forEach(function (location) {
-      $("#" + IDify(location.name) + " .options").append("<button type=\"button\" id=\"move-" + IDify(location.name) + "\">Move</button>");
-    });
-    // Create Cure button for player location
-    var disabled = "disabled";
-    if (game.player.treated[playerLocation.infestationDefault]) {
-      disabled = null;
-    }
-    $("#" + IDify(playerLocation.name) + " .options").append("<button type=\"button\" id=\"cure\" " + disabled + ">Cure " + playerLocation.infestationDefault + "</button>");
-    // Create Treat button for any present infestations
-    Object.keys(playerLocation.infestationAmounts).forEach(function (infestation) {
-      if (playerLocation.infestationAmounts[infestation] > 0) {
-        $("#" + IDify(playerLocation.name) + " .options").append("<button type=\"button\" id=\"treat-" + IDify(infestation) + "\">Treat " + infestation + "</button>");
-      }
-    });
+    // Object.keys(game.locations).forEach(function(name) {
+    //   let location = game.locations[name];
+    //   $(`#${IDify(name)}`).append(`<ul></ul>`);
+    //   Object.keys(location.infestationAmounts).forEach(function(infestation) {
+    //     if(location.infestationAmounts[infestation] > 0) {
+    //       $(`#${IDify(name)} ul`).append(`<li>${infestation}: ${location.infestationAmounts[infestation]}</li>`)
+    //     }
+    //   });
+    // });
+    // let playerLocation = game.player.location;
+    // // Highlight player location
+    // $(`#${IDify(playerLocation.name)}`).addClass("player-location");
+    // // Add Move option to next door locations
+    // playerLocation.nextDoor.forEach(function(location) {
+    //   $(`#${IDify(location.name)} .options`).append(`<button type="button" id="move-${IDify(location.name)}">Move</button>`);
+    // });
+    // // Create Cure button for player location
+    // let disabled = "disabled";
+    // if (game.player.treated[playerLocation.infestationDefault]) {
+    //   disabled = null;
+    // }
+    // $(`#${IDify(playerLocation.name)} .options`).append(`<button type="button" id="cure" ${disabled}>Cure ${playerLocation.infestationDefault}</button>`);
+    // // Create Treat button for any present infestations
+    // Object.keys(playerLocation.infestationAmounts).forEach(function(infestation) {
+    //   if (playerLocation.infestationAmounts[infestation] > 0) {
+    //     $(`#${IDify(playerLocation.name)} .options`).append(`<button type="button" id="treat-${IDify(infestation)}">Treat ${infestation}</button>`);
+    //   }
+    // });
   });
 });
 
--- js/pandemic-interface.js
+++ js/pandemic-interface.js
@@ -1,47 +1,92 @@
 import { Game } from './../js/game.js';
 
+const timeUnit = 500;
+
 function IDify(string) {
   string = string.split(" ").join("-");
   return string;
 }
 
-$(document).ready(function() {
-  const game = new Game(15000);
+function clearUI() {
+  $(".options").html("");
+  $("ul ul").html("");
+  $(".player-location").removeClass("player-location");
+}
+
+function updateUI(game) {
+  console.log(game);
+  clearUI();
+  // Show infestation amounts for each location
   Object.keys(game.locations).forEach(function(name) {
-    $('.locations').append(`<li id=${IDify(name)}>${name}<div class="options"> </div></li>`);
+    let location = game.locations[name];
+    Object.keys(location.infestationAmounts).forEach(function(infestation) {
+      if(location.infestationAmounts[infestation] > 0) {
+        $(`#${IDify(name)} ul`).append(`<li>${infestation}: ${location.infestationAmounts[infestation]}</li>`)
+      }
+    });
+  });
+  let playerLocation = game.player.location;
+  // Highlight player location
+  $(`#${IDify(playerLocation.name)}`).addClass("player-location");
+  // Add Move option to next door locations
+  playerLocation.nextDoor.forEach(function(location) {
+    $(`#${IDify(location.name)} .options`).append(`<button type="button" id="move-${IDify(location.name)}">Move</button>`);
+  });
+  // Create Cure button for player location
+  let disabled = "disabled";
+  if (game.player.treated[playerLocation.infestationDefault]) {
+    disabled = null;
+  }
+  $(`#${IDify(playerLocation.name)} .options`).append(`<button type="button" id="cure" ${disabled}>Cure ${playerLocation.infestationDefault}</button>`);
+  // Create Treat button for any present infestations
+  Object.keys(playerLocation.infestationAmounts).forEach(function(infestation) {
+    if (playerLocation.infestationAmounts[infestation] > 0) {
+      $(`#${IDify(playerLocation.name)} .options`).append(`<button type="button" id="treat-${IDify(infestation)}">Treat ${infestation}</button>`);
+    }
+  });
+}
+
+$(document).ready(function() {
+  const game = new Game(timeUnit);
+  Object.keys(game.locations).forEach(function(name) {
+    $('.locations').append(`<li id=${IDify(name)}>${name}<div class="options"> </div><ul></ul></li>`);
   });
 
   $('#game-start').click(function() {
     game.start();
     console.log(game);
+    updateUI(game);
+    setInterval(function() {
+      updateUI(game);
+    }, timeUnit);
     // Show infestation amounts for each location
-    Object.keys(game.locations).forEach(function(name) {
-      let location = game.locations[name];
-      $(`#${IDify(name)}`).append(`<ul></ul>`);
-      Object.keys(location.infestationAmounts).forEach(function(infestation) {
-        if(location.infestationAmounts[infestation] > 0) {
-          $(`#${IDify(name)} ul`).append(`<li>${infestation}: ${location.infestationAmounts[infestation]}</li>`)
-        }
-      });
-    });
-    let playerLocation = game.player.location;
-    // Highlight player location
-    $(`#${IDify(playerLocation.name)}`).addClass("player-location");
-    // Add Move option to next door locations
-    playerLocation.nextDoor.forEach(function(location) {
-      $(`#${IDify(location.name)} .options`).append(`<button type="button" id="move-${IDify(location.name)}">Move</button>`);
-    });
-    // Create Cure button for player location
-    let disabled = "disabled";
-    if (game.player.treated[playerLocation.infestationDefault]) {
-      disabled = null;
-    }
-    $(`#${IDify(playerLocation.name)} .options`).append(`<button type="button" id="cure" ${disabled}>Cure ${playerLocation.infestationDefault}</button>`);
-    // Create Treat button for any present infestations
-    Object.keys(playerLocation.infestationAmounts).forEach(function(infestation) {
-      if (playerLocation.infestationAmounts[infestation] > 0) {
-        $(`#${IDify(playerLocation.name)} .options`).append(`<button type="button" id="treat-${IDify(infestation)}">Treat ${infestation}</button>`);
-      }
-    })
+    // Object.keys(game.locations).forEach(function(name) {
+    //   let location = game.locations[name];
+    //   $(`#${IDify(name)}`).append(`<ul></ul>`);
+    //   Object.keys(location.infestationAmounts).forEach(function(infestation) {
+    //     if(location.infestationAmounts[infestation] > 0) {
+    //       $(`#${IDify(name)} ul`).append(`<li>${infestation}: ${location.infestationAmounts[infestation]}</li>`)
+    //     }
+    //   });
+    // });
+    // let playerLocation = game.player.location;
+    // // Highlight player location
+    // $(`#${IDify(playerLocation.name)}`).addClass("player-location");
+    // // Add Move option to next door locations
+    // playerLocation.nextDoor.forEach(function(location) {
+    //   $(`#${IDify(location.name)} .options`).append(`<button type="button" id="move-${IDify(location.name)}">Move</button>`);
+    // });
+    // // Create Cure button for player location
+    // let disabled = "disabled";
+    // if (game.player.treated[playerLocation.infestationDefault]) {
+    //   disabled = null;
+    // }
+    // $(`#${IDify(playerLocation.name)} .options`).append(`<button type="button" id="cure" ${disabled}>Cure ${playerLocation.infestationDefault}</button>`);
+    // // Create Treat button for any present infestations
+    // Object.keys(playerLocation.infestationAmounts).forEach(function(infestation) {
+    //   if (playerLocation.infestationAmounts[infestation] > 0) {
+    //     $(`#${IDify(playerLocation.name)} .options`).append(`<button type="button" id="treat-${IDify(infestation)}">Treat ${infestation}</button>`);
+    //   }
+    // });
   });
 });
